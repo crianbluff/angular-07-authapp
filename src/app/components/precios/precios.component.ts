7b05c656492d4d169541b7d711a5ccb60d4a9350
crianbluff <crianbluff77@gmail.com>
2019-02-12 11:10:25 -0500

    Proyecto Angular AuthApp

---
+++ src/app/components/precios/precios.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-precios',
+  template: '<h1>Precios</h1>'
+})
+export class PreciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
